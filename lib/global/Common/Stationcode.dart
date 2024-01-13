9eb23b0490812825439f084b5a7730abbca5862b
Kiran P <97682102+K-i-r-a-n-p@users.noreply.github.com>
2024-01-13 23:34:18 +0530

    added station codes

---
+++ lib/global/Common/Stationcode.dart
@@ -0,0 +1,15 @@
+Map<String, String> stationCodes = {
+  'Thrissur': 'TCR',
+  'Kozhikode': 'CLT',
+  'Thiruvananthapuram': 'TVC',
+  'Aluva': 'AWY',
+  'Kollam': 'QLN',
+  'Kayamkulam': 'KYJ',
+  'Shornur': 'SRR',
+  'Ernakulam': 'ERN',
+  'Kottayam': 'KTYM',
+  'Alappuzha': 'ALLP',
+  'Ottappalam': 'OTP',
+  'Guruvayur': 'GUV',
+  'Palakkad': 'PGTN',
+};
