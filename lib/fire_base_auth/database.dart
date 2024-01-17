a5143645a4a6093005f17acfe8ddb46de40ed12a
Kiran P <97682102+K-i-r-a-n-p@users.noreply.github.com>
2024-01-17 23:04:24 +0530

    created a databse for bookings

--- lib/fire_base_auth/database.dart
+++ lib/fire_base_auth/database.dart
@@ -7,6 +7,9 @@ class DatabaseService {
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection("Users");
 
+  final CollectionReference bookingCollection =
+      FirebaseFirestore.instance.collection("Bookings");
+
   Future<void> createUserData(
       String name, String mobile, String email, String password) async {
     return await userCollection.doc(uid).set({
@@ -15,5 +18,34 @@ class DatabaseService {
       'email': email,
       'password': password,
     });
+  }
+
+  Future<DocumentReference<Map<String, dynamic>>> createBookingData(
+      String from, String to, String date, String transportMode) async {
+    return await bookingCollection.doc(uid).collection("UserBookings").add({
+      'from': from,
+      'to': to,
+      'date': date,
+      'transportMode': transportMode,
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> getUserBookings() async {
+    QuerySnapshot querySnapshot =
+        await bookingCollection.doc(uid).collection("UserBookings").get();
+
+    return querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
+
+  Future<void> clearUserBookings() async {
+    QuerySnapshot<Map<String, dynamic>> userBookingsSnapshot =
+        await bookingCollection.doc(uid).collection("UserBookings").get();
+
+    for (QueryDocumentSnapshot<Map<String, dynamic>> documentSnapshot
+        in userBookingsSnapshot.docs) {
+      await documentSnapshot.reference.delete();
+    }
   }
 }
